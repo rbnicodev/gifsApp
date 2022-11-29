45fd2d65889a5f6e8b109d4ed8f61871dac462fe
rbnicodev <rubenmsanchis.dev@gmail.com>
2022-11-29 19:01:52 +0100

    07-11 ApiKey

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -6,14 +6,23 @@ import { Injectable } from '@angular/core';
 export class GifsService {
 
   private _history: string[] = [];
+  private apiKey: string = 'dLq0RY1bpDo199Aw3e4FSpEcCpTghEq7';
 
   get history() {
     return [...this._history]
   }
 
   searchGifs ( query: string ) {
-    if (query.length > 0) this._history.unshift( query );
-    if (this._history.length > 10) this._history.pop();
+
+    query = query.trim().toLocaleLowerCase();
+
+    if (query.trim().length === 0) return;
+    if (this._history.includes(query)) return;
+    
+    this._history.unshift( query );
+
+    if (this._history.length > 10) this._history.splice(0, 10);
+
     console.log(this._history);
   }
 }
