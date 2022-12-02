46dfee7e4074904a403d99516d4bcc3898e66faf
rbnicodev <rubenmsanchis.dev@gmail.com>
2022-12-02 17:20:16 +0100

    07-12 - Realizar petición HTTP con HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { GifsModule } from './gifs/gifs.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     SharedModule,
     GifsModule
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -12,6 +13,8 @@ export class GifsService {
     return [...this._history]
   }
 
+  constructor ( private http: HttpClient ) {}
+
   searchGifs ( query: string ) {
 
     query = query.trim().toLocaleLowerCase();
@@ -23,6 +26,10 @@ export class GifsService {
 
     if (this._history.length > 10) this._history.splice(0, 10);
 
-    console.log(this._history);
+    this.http.get('http://api.giphy.com/v1/gifs/search?api_key=dLq0RY1bpDo199Aw3e4FSpEcCpTghEq7&q=goku&limit=10')
+      .subscribe( (resp:any) => {
+        console.log(resp.data);
+      } );
+    
   }
 }
